6e2af9ee4ec15805ab68ae0ace6471fb651c46f8
Andriy Kutushonok <kutushonok593@gmail.com>
2021-11-04 14:12:06 +0200

    js-hw-13

---
+++ src/partials/apiService.js
@@ -0,0 +1,6 @@
+export default function showCountries(inputValue, number) {
+    return fetch(`https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${inputValue}&page=${number}&per_page=12&key=24182944-6a44f26d4ebf1d494c89c0c95`)
+         .then(response => {
+             return response.json();
+         })
+ };
